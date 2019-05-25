5be9a0bfa462b08c683743d3346e0373261fcb18
D'Quavius Griffin <dquaviusgriffin@hotmail.com>
2019-05-24 23:26:20 -0400

    Initialize label

--- Typerun/TextField.cpp
+++ Typerun/TextField.cpp
@@ -6,10 +6,11 @@ TextField::TextField()
 {
 }
 
-
-TextField::~TextField()
+TextField::TextField(sf::Vector2f position, sf::Color text_color, sf::Font& font) : text_color{text_color}, font{font}
 {
+	label = Label("TextField", "Hello", font, 22, text_color, sf::Text::Regular, position);
 }
+
 
 void TextField::add_character(char character)
 {
@@ -21,11 +22,19 @@ void TextField::add_character(char character)
 void TextField::remove_last_character()
 {
 	std::string temp = label.get_drawable().getString().toAnsiString();
-	temp.pop_back();
-	label.set_text(temp);
+	if (temp != "")
+	{
+		temp.pop_back();
+		label.set_text(temp);
+	}
 }
 
 std::string TextField::get_text()
 {
 	return label.get_drawable().getString().toAnsiString();
+}
+
+sf::Text & TextField::get_drawable()
+{
+	return label.get_drawable();
 }
--- Typerun/TextField.h
+++ Typerun/TextField.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "Label.h"
+#include "SFML/Graphics/Color.hpp"
+#include "SFML/Graphics/Font.hpp"
+#include "SFML/System/Vector2.hpp"
 
 class TextField
 {
 private:
 	Label label;
+	sf::Color text_color;
+	sf::Font font;
 public:
 	TextField();
-	~TextField();
+	TextField(sf::Vector2f position, sf::Color text_color, sf::Font& font);
 	void add_character(char character);
 	void remove_last_character();
 	std::string get_text();
+	sf::Text& get_drawable();
 };
 
