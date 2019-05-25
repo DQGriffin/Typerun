b7bdc646b717de353f2434fde5b4275c5efd5021
D'Quavius Griffin <dquaviusgriffin@hotmail.com>
2019-05-24 21:48:12 -0400

    Implement copy assignment operator

--- Typerun/Word.h
+++ Typerun/Word.h
@@ -14,11 +14,14 @@ private:
 	int id;
 	const int min_id = 11111;
 	const int max_id = 99999;
+	bool alive;
 public:
 	Word(std::string value, sf::Vector2f position, sf::Color initial_color, sf::Font& font);
+	Word& operator=(Word w);
 	void update();
 	sf::Text& get_drawable();
 	int get_id();
+	bool is_alive();
 private:
 	void update_position();
 	void update_color();
